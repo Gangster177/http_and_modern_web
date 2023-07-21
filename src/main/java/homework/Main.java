fb94fad91e87a39845bce17efb5d80aa1954913b
Nikita Novikov <monstars51@gmail.com>
2023-07-21 21:22:43 +0300

    add Handler.java

--- src/main/java/homework/Main.java
+++ src/main/java/homework/Main.java
@@ -1,9 +1,28 @@
 package homework;
 
+import java.io.BufferedOutputStream;
+import java.util.logging.Handler;
 
 public class Main {
+    private static final int SERVER_PORT = 9999;
+    private static final int THREAD_POOL_SIZE = 64;
+
     public static void main(String[] args) {
-        Server server = new Server();
-        server.start(9999);
+        Server server = new Server(THREAD_POOL_SIZE);
+
+
+//        server.addHandler("GET", "/messages", new Handler() {
+//            public void handle(Request request, BufferedOutputStream responseStream) {
+//                // TODO: handlers code
+//            }
+//        });
+//        server.addHandler("POST", "/messages", new Handler() {
+//            public void handle(Request request, BufferedOutputStream responseStream) {
+//                // TODO: handlers code
+//            }
+//        });
+
+
+        server.start(SERVER_PORT);
     }
 }
--- src/main/java/homework/Server.java
+++ src/main/java/homework/Server.java
@@ -4,6 +4,7 @@ import java.net.Socket;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.time.LocalDateTime;
+import java.util.HashMap;
 import java.util.List;
 
 import java.io.BufferedOutputStream;
@@ -11,11 +12,14 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.logging.Handler;
 
 public class Server {
-    final List<String> validPaths = List.of("/index.html",
+    private final List<String> validPaths = List.of("/index.html",
             "/spring.svg",
             "/spring.png",
             "/resources.html",
@@ -26,87 +30,104 @@ public class Server {
             "/classic.html",
             "/events.html",
             "/events.js");
-    ExecutorService es = Executors.newFixedThreadPool(64);
-    static Socket socket;
+    private final ExecutorService es;
+    private static Socket socket;
+    private final ConcurrentHashMap<String, Map<String, Handler>> handlers;
+
+    public Server(int poolSize) {
+        this.es = Executors.newFixedThreadPool(poolSize);
+        this.handlers = new ConcurrentHashMap<>();
+    }
 
     public void start(int port) {
-        try (final var serverSocket = new ServerSocket(port)) {
+        try (final ServerSocket serverSocket = new ServerSocket(port)) {
             while (true) {
                 socket = serverSocket.accept();
                 es.submit(() -> newConnect(socket));
             }
         } catch (IOException e) {
             e.printStackTrace();
+        } finally {
+            es.shutdown();
         }
     }
 
     private void newConnect(Socket socket) {
         try (
-             final BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-             final BufferedOutputStream out = new BufferedOutputStream(socket.getOutputStream())
+                final BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                final BufferedOutputStream out = new BufferedOutputStream(socket.getOutputStream())
         ) {
             final String requestLine = in.readLine();
             final String[] parts = requestLine.split(" ");
 
             if (parts.length != 3) {
-                out.write((
-                        "HTTP/1.1 400 Bad Request\r\n" +
-                                "Content-Length: 0\r\n" +
-                                "Connection: close\r\n" +
-                                "\r\n"
-                ).getBytes());
-                out.flush();
                 return;
             }
 
-            final String path = parts[1];
-            if (!validPaths.contains(path)) {
-                out.write((
-                        "HTTP/1.1 404 Not Found\r\n" +
-                                "Content-Length: 0\r\n" +
-                                "Connection: close\r\n" +
-                                "\r\n"
-                ).getBytes());
-                out.flush();
+            Request request = new Request(parts);
+
+            if (!validPaths.contains(request.getPath())) {
+                badRequest(out);
                 return;
             }
+            response(out,request);
 
+        } catch (
+                IOException e) {
+           // e.printStackTrace();
+        }
+    }
 
-            final Path filePath = Path.of(".", "public", "resources", path);
-            final String mimeType = Files.probeContentType(filePath);
+    void badRequest(BufferedOutputStream out) throws IOException {
+        out.write((
+                "HTTP/1.1 400 Bad Request\r\n" +
+                        "Content-Length: 0\r\n" +
+                        "Connection: close\r\n" +
+                        "\r\n"
+        ).getBytes());
+        out.flush();
+    }
 
-            // special case for classic
-            if (path.equals("/classic.html")) {
-                final String template = Files.readString(filePath);
-                final byte[] content = template.replace(
-                        "{time}",
-                        LocalDateTime.now().toString()
-                ).getBytes();
-                out.write((
-                        "HTTP/1.1 200 OK\r\n" +
-                                "Content-Type: " + mimeType + "\r\n" +
-                                "Content-Length: " + content.length + "\r\n" +
-                                "Connection: close\r\n" +
-                                "\r\n"
-                ).getBytes());
-                out.write(content);
-                out.flush();
-                return;
-            }
+    void response(BufferedOutputStream out, Request request) throws IOException {
+        final Path filePath = Path.of(".", "public", "resources", request.getPath());
+        final String mimeType = Files.probeContentType(filePath);
 
-            final long length = Files.size(filePath);
+        // special case for classic
+        if (request.getPath().equals("/classic.html")) {
+            final String template = Files.readString(filePath);
+            final byte[] content = template.replace(
+                    "{time}",
+                    LocalDateTime.now().toString()
+            ).getBytes();
             out.write((
                     "HTTP/1.1 200 OK\r\n" +
                             "Content-Type: " + mimeType + "\r\n" +
-                            "Content-Length: " + length + "\r\n" +
+                            "Content-Length: " + content.length + "\r\n" +
                             "Connection: close\r\n" +
                             "\r\n"
-            ).getBytes()); // начальная часть запроса отправляется в выходной поток
-            Files.copy(filePath, out); // копируем файл по байтам в выходной поток
+            ).getBytes());
+            out.write(content);
             out.flush();
-        } catch (
-                IOException e) {
-            e.printStackTrace();
+            return;
         }
+
+        final long length = Files.size(filePath);
+        out.write((
+                "HTTP/1.1 200 OK\r\n" +
+                        "Content-Type: " + mimeType + "\r\n" +
+                        "Content-Length: " + length + "\r\n" +
+                        "Connection: close\r\n" +
+                        "\r\n"
+        ).getBytes()); // начальная часть запроса отправляется в выходной поток
+        Files.copy(filePath, out); // копируем файл по байтам в выходной поток
+        out.flush();
     }
+
+    public void addHandler(String method, String path, Handler handler) {
+        if (!handlers.contains(method)) {
+            handlers.put(method, new HashMap<>());
+        }
+        handlers.get(method).put(path, handler);
+    }
+
 }
